f72ebc316b673c57f3bfb55ff04b28492f1beb15
RobertoJCastillo <roberto_16051995@hotmail.com>
2023-03-17 15:45:18 -0600

    Mensaje

---
+++ src/main/java/com/api_gitflow/api_gitflow/dao/Estudiante.java
@@ -0,0 +1,9 @@
+package com.api_gitflow.api_gitflow.dao;
+
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+public class Estudiante {
+    public void mensaje(){
+        System.out.println("mensaje");
+    }
+}
